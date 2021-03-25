4c86c7c5ce7ea9f85afef3eafd4c6d6ecdeb79d3
msperlin <marceloperlin@gmail.com>
2021-03-25 10:52:27 -0300

    new bash script

--- apt-to-install/install_by_apt.sh
+++ apt-to-install/install_by_apt.sh
@@ -9,7 +9,7 @@ dpkg -s $apt_soft &> /dev/null
 if [ $? -ne 0 ]
    then
        echo "  - Not installed, installing it"  
-       sudo apt-get install $name -y &> /dev/null
+       sudo apt-get install $apt_soft -y &> /dev/null
 
    else
        echo    "  - already installed"
