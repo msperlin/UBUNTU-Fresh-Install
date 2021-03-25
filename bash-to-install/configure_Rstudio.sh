b975833bff13823829a57325e9d83948b497753a
msperlin <marceloperlin@gmail.com>
2021-03-25 11:45:02 -0300

    new R pkg script

--- bash-to-install/configure_Rstudio.sh
+++ bash-to-install/configure_Rstudio.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # set rstudio config
-rstudio_config="bash-to-install/Rstudio-Config/my-rstudio-prefs.json"
+rstudio_config="bash-to-install/Rstudio-Config/rstudio-prefs.json"
 target_config="/home/$USER/.config/rstudio/"
+
+mkdir $target_config
 
 cp -f "$rstudio_config" "$target_config" 
