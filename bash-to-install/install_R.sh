3c948787a354b76c378c9f17192b85f9f775095e
msperlin <marceloperlin@gmail.com>
2022-05-18 11:17:28 -0300

    new apt package python3-pip

--- bash-to-install/install_R.sh
+++ bash-to-install/install_R.sh
@@ -3,17 +3,17 @@
 # defines ubuntu codename for CRAN repo 
 # see: https://cloud.r-project.org/bin/linux/ubuntu/
 # see: https://www.linuxmint.com/download_all.php
-ubuntu_codename="focal"
+ubuntu_codename="focal" # or jammy for ubuntu 22.04
 
 echo "  - Installing R"
 
-# new ver (2021-09-13) - 
+# new ver (2022-05-18) - 
 # https://cloud.r-project.org/bin/linux/ubuntu/#install-r
-## Hirsute Hippo (21.04, amd64 only)
-## Groovy Gorilla (20.10, amd64 only),
-## Focal Fossa (20.04; LTS and amd64 only),
-## Bionic Beaver (18.04; LTS), and
-## Xenial Xerus (16.04; LTS).
+##  Jammy Jellyfish (22.04, amd64 only)
+##  Impish Indri (21.10, amd64 only),
+##  Focal Fossa (20.04; LTS and amd64 only),
+##  Bionic Beaver (18.04; LTS), and
+##  Xenial Xerus (16.04; LTS).
 
 # update indices
 sudo apt update -qq
@@ -24,9 +24,10 @@ sudo apt install --no-install-recommends software-properties-common dirmngr
 # To verify key, run gpg --show-keys /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc 
 # Fingerprint: 298A3A825C0D65DFD57CBB651716619E084DAB9
 wget -qO- https://cloud.r-project.org/bin/linux/ubuntu/marutter_pubkey.asc | sudo tee -a /etc/apt/trusted.gpg.d/cran_ubuntu_key.asc
+
 # add the R 4.0 repo from CRAN -- adjust 'focal' to 'groovy' or 'bionic' as needed
 sudo add-apt-repository "deb https://cloud.r-project.org/bin/linux/ubuntu $ubuntu_codename-cran40/"
 
 sudo apt update
 
-sudo apt install --no-install-recommends r-base r-base-dev
+sudo apt install --no-install-recommends r-base r-base-dev -y 
