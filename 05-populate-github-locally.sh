ab11b0858c57415ad5580d4919d20b16597fed94
msperlin <marceloperlin@gmail.com>
2022-10-21 15:35:28 -0300

    new scripts improv

--- 05-populate-github-locally.sh
+++ 05-populate-github-locally.sh
@@ -1,11 +1,75 @@
 #!/usr/bin/env bash
 
-# Bash Script for installing r packages in file pkgs_to_install.txt
+# populates gitrepo directory 
+# make sure you have ssh registered at github
 
-echo "Installing latex"
+USERNAME="msperlin"
+name_main_dir="GitRepo2"
+echo "Populating gitrepo folder"
 
-sudo apt install texlive-full texstudio
 
-sudo add-apt-repository ppa:sunderme/texstudio
-sudo apt update
-sudo apt install texstudio
+cd /home/$USERNAME/
+
+mkdir $name_main_dir
+cd $name_main_dir
+
+# clone r pkgs
+name_dir="01-r-pkgs"
+my_repos=( "GetDFPData2" "vdr" "GetFREData" "classtools" )
+
+mkdir $name_dir
+cd $name_dir
+
+for i_repo in "${my_repos[@]}"
+do
+	git clone git@github.com:msperlin/$i_repo.git
+done
+
+# other pgks
+git clone git@github.com:ropensci/yfR.git
+
+cd ..
+# books
+name_dir="02-books"
+
+my_repos=( "chapter-dataviz-saude.gov" )
+
+mkdir $name_dir
+cd $name_dir
+
+for i_repo in "${my_repos[@]}"
+do
+	git clone git@github.com:msperlin/$i_repo.git
+done
+
+cd ..
+
+# sites
+name_dir="03-sites"
+
+my_repos=( "msperlin-blog" )
+
+mkdir $name_dir
+cd $name_dir
+
+for i_repo in "${my_repos[@]}"
+do
+	git clone git@github.com:msperlin/$i_repo.git
+done
+
+cd ..
+
+# others
+name_dir="04-others"
+
+my_repos=( "UBUNTU-Fresh-Install" )
+
+mkdir $name_dir
+cd $name_dir
+
+for i_repo in "${my_repos[@]}"
+do
+	git clone git@github.com:msperlin/$i_repo.git
+done
+
+cd ..
